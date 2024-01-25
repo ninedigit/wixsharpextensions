9c9e59a8e11486c7870feb865a875d8deee936d2
Juraj Malach <juraj.malach@gmail.com>
2024-01-25 09:13:49 +0100

    ssl.com code signing added.

---
+++ NineDigit.WixSharpExtensions/CodeSigning/CodeSignToolExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using WixSharp;
+using WixSharp.CommonTasks;
+
+namespace NineDigit.WixSharpExtensions
+{
+    public static class CodeSignToolExtensions
+    {
+        /// <summary>
+        /// Signs file specified by its path using SingTool.
+        /// </summary>
+        /// <param name="fileToSign">File to sign.</param>
+        /// <param name="signOptions">SignTool sign options.</param>
+        /// <returns></returns>
+        public static string SignWithCodeSignTool(this string fileToSign, SignToolSignOptions signOptions)
+        {
+            // CodeSignTool sign -input_file_path=$(ProjectDir)$(OutputPath)$(AssemblyName).exe -credential_id=$(CodeSignToolCredentialId) -username $(CodeSignToolUserName) -password &quot;$(CodeSignToolPassword)&quot; -override -totp_secret=&quot;$(CodeSignToolTotpSecret)&quot;"
+            var args = $"sign -input_file_path=\"{fileToSign}\" -username \"{signOptions.UserName}\" -password \"{signOptions.Password}\" -override -totp_secret=\"{signOptions.TotpSecret}\"";
+            
+            if (!string.IsNullOrWhiteSpace(signOptions.CredentialId))
+                args += $" -credential_id=\"{signOptions.CredentialId}\"";
+
+            var tool = new ExternalTool
+            {
+                // Note: As CodeSignTool should be added to system path, we are not using wellKnownLocations here.
+                
+                //WellKnownLocations = wellKnownLocations ??
+                //                     @"C:\Program Files (x86)\Windows Kits\10\App Certification Kit;" +
+                //                     @"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86;" +
+                //                     @"C:\Program Files (x86)\Windows Kits\10\bin\x86;" +
+                //                     @"C:\Program Files (x86)\Windows Kits\8.1\bin\x86;" +
+                //                     @"C:\Program Files (x86)\Windows Kits\8.0\bin\x86;" +
+                //                     @"C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool;" +
+                //                     @"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin;" +
+                //                     @"C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin",
+                ExePath = "CodeSignTool.bat",
+                Arguments = args
+            };
+
+            Console.WriteLine($"Signing '{fileToSign}' using SignTool.");
+
+            if (signOptions.Password.IsNotEmpty() || signOptions.TotpSecret.IsNotEmpty())
+                tool.ConsoleOut = (line) => Compiler.OutputWriteLine(line.Replace(signOptions.Password, "***").Replace(signOptions.TotpSecret, "***"));
+
+            var returnCode = tool.ConsoleRun();
+            if (returnCode == 0)
+                Console.WriteLine($"File '{fileToSign}' successfully signed using SignTool.");
+            else
+                Console.WriteLine($"File '{fileToSign}' signing using SignTool failed. Return code: {returnCode}");
+
+            return fileToSign;
+        }
+    }
+}
+
---
+++ NineDigit.WixSharpExtensions/CodeSigning/SignToolSignOptions.cs
@@ -0,0 +1,80 @@
+﻿using System.Xml;
+
+namespace NineDigit.WixSharpExtensions
+{
+    public class SignToolSignOptions
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="userName">SSL.com account username</param>
+        /// <param name="password">SSL.com account password.</param>
+        /// <param name="totpSecret">The TOTP (time-based one-time password) secret value associated with your eSigner certificate.</param>
+        /// <param name="credentialId">Credential ID for signing certificate. Required only for users with more than one eSigner code signing certificate.</param>
+        /// <exception cref="System.ArgumentException"></exception>
+        public SignToolSignOptions(string userName, string password, string totpSecret, string? credentialId)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+                throw new System.ArgumentException($"'{nameof(userName)}' cannot be null or whitespace.", nameof(userName));
+            if (string.IsNullOrWhiteSpace(password))
+                throw new System.ArgumentException($"'{nameof(password)}' cannot be null or whitespace.", nameof(password));
+            if (string.IsNullOrWhiteSpace(totpSecret))
+                throw new System.ArgumentException($"'{nameof(totpSecret)}' cannot be null or whitespace.", nameof(totpSecret));
+
+            UserName = userName;
+            Password = password;
+            TotpSecret = totpSecret;
+            CredentialId = credentialId;
+        }
+
+        /// <summary>
+        /// SSL.com account username
+        /// </summary>
+        public string UserName { get; }
+        /// <summary>
+        /// SSL.com account password.
+        /// </summary>
+        public string Password { get; }
+        /// <summary>
+        /// The TOTP (time-based one-time password) secret value associated with your eSigner certificate. 
+        /// </summary>
+        public string TotpSecret { get; }
+        /// <summary>
+        /// Credential ID for signing certificate.
+        /// </summary>
+        public string? CredentialId { get; }
+
+        /// <summary>
+        /// Creates new instance based on parsed values from file with following XML structure:
+        /// <code>
+        /// <![CDATA[
+        /// <Project>
+        ///     <PropertyGroup>
+        ///         <CodeSignToolCredentialId></CodeSignToolCredentialId>
+        ///         <CodeSignToolUserName></CodeSignToolUserName>
+        ///         <CodeSignToolPassword></CodeSignToolPassword>
+        ///         <CodeSignToolTotpSecret></CodeSignToolTotpSecret>
+        ///     </PropertyGroup>
+        /// </Project>
+        /// ]]>
+        /// </code>
+        /// </summary>
+        /// <param name="filePath">Path to the file with XML content.</param>
+        /// <returns></returns>
+        public static SignToolSignOptions FromPropsFile(string filePath)
+        {
+            if (!System.IO.File.Exists(filePath))
+                throw new System.InvalidOperationException($"The file '{filePath}' was not found.");
+
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(filePath);
+
+            var userName = xmlDocument.SelectSingleNode("//Project/PropertyGroup/CodeSignToolUserName")?.InnerText;
+            var password = xmlDocument.SelectSingleNode("//Project/PropertyGroup/CodeSignToolPassword")?.InnerText;
+            var totpSecret = xmlDocument.SelectSingleNode("//Project/PropertyGroup/CodeSignToolTotpSecret")?.InnerText;
+            var credentialId = xmlDocument.SelectSingleNode("//Project/PropertyGroup/CodeSignToolCredentialId")?.InnerText;
+
+            return new SignToolSignOptions(userName, password, totpSecret, credentialId);
+        }
+    }
+}
+
